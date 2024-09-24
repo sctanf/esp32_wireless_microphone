d13fe25ea82a6e3dfa97fb882f1d9daf3696f335
sctanf <36978460+sctanf@users.noreply.github.com>
2024-09-23 19:02:21 -0500

    update configs

--- firmware/src/config.cpp
+++ firmware/src/config.cpp
@@ -20,3 +20,22 @@ i2s_pin_config_t i2s_mic_pins = {
     .ws_io_num = I2S_MIC_LEFT_RIGHT_CLOCK,
     .data_out_num = I2S_PIN_NO_CHANGE,
     .data_in_num = I2S_MIC_SERIAL_DATA};
+
+i2s_config_t i2s_out_Config = {
+    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
+    .sample_rate = SAMPLE_RATE,
+    .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
+    .channel_format = I2S_OUT_CHANNEL,
+    .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+    .dma_buf_count = 16,
+    .dma_buf_len = 512,
+    .use_apll = true,
+    .tx_desc_auto_clear = true,
+    .fixed_mclk = 0};
+
+i2s_pin_config_t i2s_out_pins = {
+    .bck_io_num = I2S_OUT_SERIAL_CLOCK,
+    .ws_io_num = I2S_OUT_LEFT_RIGHT_CLOCK,
+    .data_out_num = I2S_OUT_SERIAL_DATA,
+    .data_in_num = I2S_PIN_NO_CHANGE};
--- firmware/src/config.h
+++ firmware/src/config.h
@@ -15,15 +15,24 @@
 #define SAMPLE_RATE 44100
 
 // I2S Microphone Settings
-#define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_RIGHT // bug in ESP_IDF, RIGHT actually means LEFT
+//#define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_RIGHT // bug in ESP_IDF, RIGHT actually means LEFT
 //#define I2S_MIC_SERIAL_CLOCK GPIO_NUM_23
 //#define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_22
 //#define I2S_MIC_SERIAL_DATA GPIO_NUM_19
+#define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_LEFT
 #define I2S_MIC_SERIAL_CLOCK 2
 #define I2S_MIC_LEFT_RIGHT_CLOCK 6
 #define I2S_MIC_SERIAL_DATA 4
+
+#define I2S_OUT_CHANNEL I2S_CHANNEL_FMT_RIGHT_LEFT
+#define I2S_OUT_SERIAL_CLOCK 15
+#define I2S_OUT_LEFT_RIGHT_CLOCK 16
+#define I2S_OUT_SERIAL_DATA 17
 
 // i2s config for reading from of I2S
 extern i2s_config_t i2s_mic_Config;
 // i2s microphone pins
 extern i2s_pin_config_t i2s_mic_pins;
+
+extern i2s_config_t i2s_out_Config;
+extern i2s_pin_config_t i2s_out_pins;
